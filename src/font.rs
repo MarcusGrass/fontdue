d0556c3b631b8e2a3e7f109d7e929643b3fcf81e
gramar <marcus.grass@gmail.com>
2022-05-03 19:20:17 +0200

    Oneshot rasterization pipe

--- src/font.rs
+++ src/font.rs
@@ -334,6 +334,51 @@ impl Font {
         })
     }
 
+    pub fn render_all<Data: Deref<Target = [u8]>>(data: Data, px: f32, settings: FontSettings) -> FontResult<Vec<(Metrics, Vec<u8>)>> {
+        let face = match Face::from_slice(&data, settings.collection_index) {
+            Ok(f) => f,
+            Err(e) => return Err(convert_error(e)),
+        };
+
+        let units_per_em = face.units_per_em() as f32;
+        // Collect all the unique codepoint to glyph mappings.
+        let glyph_count = face.number_of_glyphs();
+        let mut v = Vec::with_capacity(glyph_count as usize);
+        let mut seen = HashSet::with_capacity(glyph_count as usize);
+        if let Some(subtable) = face.tables().cmap {
+            for subtable in subtable.subtables {
+                subtable.codepoints(|codepoint| {
+                    if let Some(mapping) = subtable.glyph_index(codepoint) {
+                        if let Some(mapping) = NonZeroU16::new(mapping.0) {
+                            if !seen.insert(mapping) {
+                                return;
+                            }
+                            let index = mapping.get();
+                            if index >= glyph_count {
+                                return;
+                            }
+
+                            let mut glyph = Glyph::default();
+                            let glyph_id = GlyphId(index);
+                            if let Some(advance_width) = face.glyph_hor_advance(glyph_id) {
+                                glyph.advance_width = advance_width as f32;
+                            }
+                            if let Some(advance_height) = face.glyph_ver_advance(glyph_id) {
+                                glyph.advance_height = advance_height as f32;
+                            }
+
+                            let mut geometry = Geometry::new(settings.scale, units_per_em);
+                            face.outline_glyph(glyph_id, &mut geometry);
+                            geometry.finalize(&mut glyph);
+                            v.push(rasterize_glyph(glyph, units_per_em, px));
+                        }
+                    }
+                })
+            }
+        }
+        Ok(v)
+    }
+
     /// Returns all valid unicode codepoints that have mappings to glyph geometry in the font, along
     /// with their associated index. This does not include grapheme cluster mappings. The mapped
     /// NonZeroU16 index can be used in the _indexed font functions.
@@ -617,4 +662,38 @@ impl Font {
     pub fn glyph_count(&self) -> u16 {
         self.glyphs.len() as u16
     }
+}
+
+fn rasterize_glyph(glyph: Glyph, units_per_em: f32, px: f32) -> (Metrics, Vec<u8>) {
+    if px <= 0.0 {
+        return (Metrics::default(), Vec::new());
+    }
+    let scale = px / units_per_em;
+    let (metrics, offset_x, offset_y) = metrics_raw(scale, &glyph, 0.0);
+    let mut canvas = Raster::new(metrics.width, metrics.height);
+    canvas.draw(&glyph, scale, scale, offset_x, offset_y);
+    (metrics, canvas.get_bitmap())
+}
+
+/// Internal function to generate the metrics, offset_x, and offset_y of the glyph.
+fn metrics_raw(scale: f32, glyph: &Glyph, offset: f32) -> (Metrics, f32, f32) {
+    let bounds = glyph.bounds.scale(scale);
+    let mut offset_x = fract(bounds.xmin + offset);
+    let mut offset_y = fract(1.0 - fract(bounds.height) - fract(bounds.ymin));
+    if is_negative(offset_x) {
+        offset_x += 1.0;
+    }
+    if is_negative(offset_y) {
+        offset_y += 1.0;
+    }
+    let metrics = Metrics {
+        xmin: as_i32(floor(bounds.xmin)),
+        ymin: as_i32(floor(bounds.ymin)),
+        width: as_i32(ceil(bounds.width + offset_x)) as usize,
+        height: as_i32(ceil(bounds.height + offset_y)) as usize,
+        advance_width: scale * glyph.advance_width,
+        advance_height: scale * glyph.advance_height,
+        bounds,
+    };
+    (metrics, offset_x, offset_y)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! This is a no_std crate, but still requires the alloc crate.
 
-#![no_std]
+//#![no_std]
 #![allow(dead_code)]
 #![allow(clippy::style)]
 #![allow(clippy::complexity)]
@@ -23,3 +23,25 @@ pub use crate::font::*;
 
 /// Alias for Result<T, &'static str>.
 pub type FontResult<T> = Result<T, &'static str>;
+
+#[cfg(test)]
+mod tests {
+    use crate::{Font, FontSettings};
+
+    #[test]
+    fn rasterize_oneshot() {
+        let jetbrains_mono = "/usr/share/fonts/TTF/JetBrains Mono Medium Nerd Font Complete.ttf";
+        let font_bytes = std::fs::read(jetbrains_mono).unwrap();
+        let rasterized = Font::render_all(font_bytes.as_slice(), 12.0, FontSettings {
+            collection_index: 0,
+            scale: 600.0
+        }).unwrap();
+        eprintln!("{}", rasterized.len());
+        drop(rasterized);
+        drop(font_bytes);
+
+        loop {
+
+        }
+    }
+}
